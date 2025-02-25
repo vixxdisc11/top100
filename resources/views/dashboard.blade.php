702b4c3c02917c83ad940ddad33568a2fb1e12a6
Angus McRitchie <53469513+angus-mcritchie@users.noreply.github.com>
2025-02-26 00:59:11 +1100

    fix pattern overflow on placeholder (#21)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,7 @@
                 <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
             </div>
         </div>
-        <div class="relative h-full flex-1 rounded-xl border border-neutral-200 dark:border-neutral-700">
+        <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
             <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
         </div>
     </div>
