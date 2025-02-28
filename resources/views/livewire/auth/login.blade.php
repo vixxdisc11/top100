6d7c47bf7ef8012b2c58c89d1931264255a6b052
Jon Morby <110604496+jpmorby@users.noreply.github.com>
2025-02-28 18:55:07 +0000

    Add check for register route in case registrations have been disabled (#37)
    * Add check for register route so if registrations have been disabled the form doesn't die
    
    * fixed indentation as requested
    
    * Update login.blade.php
    
    ---------
    
    Co-authored-by: Jon Morby <jon@morby.org>
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -3,6 +3,7 @@
 use Illuminate\Auth\Events\Lockout;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
@@ -117,9 +118,10 @@ new #[Layout('components.layouts.auth')] class extends Component {
             <flux:button variant="primary" type="submit" class="w-full">{{ __('Log in') }}</flux:button>
         </div>
     </form>
-
-    <div class="space-x-1 text-center text-sm text-zinc-600 dark:text-zinc-400">
-        Don't have an account?
-        <flux:link href="{{ route('register') }}" wire:navigate>Sign up</flux:link>
-    </div>
+    @if (Route::has('register'))
+        <div class="space-x-1 text-center text-sm text-zinc-600 dark:text-zinc-400">
+            Don't have an account?
+            <flux:link href="{{ route('register') }}" wire:navigate>Sign up</flux:link>
+        </div>
+    @endif
 </div>
