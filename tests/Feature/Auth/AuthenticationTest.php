266aa0f7097159a96b82eb0345bfcea1bceb82fc
Sergio Peris <dev.sertxu@gmail.com>
2025-03-19 15:25:22 +0100

    Fix test `users_can_not_authenticate_with_invalid_password` (#64)
    * Update AuthenticationTest.php
    
    * Update AuthenticationTest.php
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -38,10 +38,12 @@ class AuthenticationTest extends TestCase
     {
         $user = User::factory()->create();
 
-        $this->post('/login', [
-            'email' => $user->email,
-            'password' => 'wrong-password',
-        ]);
+        $response = LivewireVolt::test('auth.login')
+            ->set('email', $user->email)
+            ->set('password', 'wrong-password')
+            ->call('login');
+
+        $response->assertHasErrors('email');
 
         $this->assertGuest();
     }
