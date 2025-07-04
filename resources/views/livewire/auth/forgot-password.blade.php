59609f2ee63f89c2154afd5d40abbd21006ec95a
Kyle Milloy <k@fanda.io>
2025-07-04 07:56:36 -0400

    chore: remove redundant "viewable" attribute (#105)

--- resources/views/livewire/auth/forgot-password.blade.php
+++ resources/views/livewire/auth/forgot-password.blade.php
@@ -37,7 +37,6 @@ new #[Layout('components.layouts.auth')] class extends Component {
             required
             autofocus
             placeholder="email@example.com"
-            viewable
         />
 
         <flux:button variant="primary" type="submit" class="w-full">{{ __('Email password reset link') }}</flux:button>
